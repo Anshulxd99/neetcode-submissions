405c2d15ff7a767722a40af4822ecebfbc33843d
Anshul <111901003+Anshulxd99@users.noreply.github.com>
2026-09-07 10:36:32 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xori=0;
+        for(auto i:nums){
+            xori^=i;
+        } 
+        return xori;
+    }
+};
